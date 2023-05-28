e321a3f0d61e508f5025c8542a487ed22c01e3ea
SushrutPola <sushrut.pola@gmail.com>
2023-05-28 16:47:15 -0700

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+  ],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
   },
+  publicDir: 'static'
 });
