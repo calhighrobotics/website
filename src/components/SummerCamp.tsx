31714f6481a8b3c651fb8ffc8fab2b3401ce3f39
Xbox <xboxbedrock@gmail.com>
2024-04-18 17:24:07 -0700

    Create SummerCamp.tsx

---
+++ src/components/SummerCamp.tsx
@@ -0,0 +1,24 @@
+import type { Component } from 'solid-js';
+
+import logo from '../roboticslogo.png';
+import '../App.module.css';
+import Navbar from './Navbar';
+
+
+
+
+const SummerCamp: Component = () => {
+  window.location.href = "https://forms.gle/6ft52bBvrqv1Uuy18"
+  
+  return (
+    
+    <div>
+        <div class="bg-black h-screen  flex"> 
+          <text class="font-manrope font-bold text-orange-600 m-auto text-9xl">Redirecting...</text>
+        </div>
+    </div>
+    
+  )
+};
+
+export default SummerCamp;
